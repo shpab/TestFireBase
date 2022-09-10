41178d27264d9fab4ad7d4417b37ceb1ff8a3576
shpab <75552289+shpab@users.noreply.github.com>
2022-09-10 11:50:56 +0900

    Initial Commit

---
+++ TestFireBase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestFireBase
+//
+//  Created by 橋本周三 on 9/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestFireBase/TestFireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFireBaseApp.swift
+//  TestFireBase
+//
+//  Created by 橋本周三 on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TestFireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
